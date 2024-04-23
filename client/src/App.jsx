e5e0b609be04dd725e3117051d47ce08dca94cc7
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-04-23 13:31:59 +0530

    complete login functionality and create private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 import Login from "./pages/Login";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 
@@ -11,11 +12,13 @@ export default function App() {
     <BrowserRouter>
 
       <Routes>
-        <Route path='/' element={<Home />} />
-       
         <Route path='/login' element={<Login />} />
 
-        
+        <Route element={<PrivateRoute />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+
+
       </Routes>
     </BrowserRouter>
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,18 @@
-export default function Home() {
+import { useSelector } from "react-redux";
 
+export default function Home() {
+    const { currentUser } = useSelector((state) => state.user);
+    console.log(currentUser);
     return (
         <div>
             <h1>Home</h1>
+            {/* display loged in user details */}
+            {currentUser && (
+                <div>
+                    <h2> current user is:{currentUser.user.basic_info.first_name}</h2>
+                    <p>email:{currentUser.user.contact_info.email}</p>
+                </div>
+            )}
             <p>Welcome to the home page!</p>
         </div>
     );
